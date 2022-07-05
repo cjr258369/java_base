b5a45738c735a3fdaf6c5374ea8f419ea697cd0e
little_jack_2014 <860681233@qq.com>
2022-07-05 17:29:24 +0800

    InterruptAPI Demo

--- java_module/src/ThreadBase/interrupt/InterruptDemo.java
+++ java_module/src/ThreadBase/interrupt/InterruptDemo.java
@@ -12,7 +12,70 @@ public class InterruptDemo {
     static AtomicBoolean atomicBoolean = new AtomicBoolean(false);
 
     public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            while(true){
+                if(Thread.currentThread().isInterrupted()){
+                    System.out.println(Thread.currentThread().getName() + "\t 中断标识："
+                            +Thread.currentThread().isInterrupted() + " ， 线程停止");
+                    break;
+                }
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("------hello InterruptDemo3");
+            }
+        }, "t1");
+        t1.start();
 
+        try {TimeUnit.SECONDS.sleep(1);} catch (InterruptedException e) {e.printStackTrace();}
+        new Thread(() -> t1.interrupt(), "t2").start();
+    }
+
+    private static void interruptDemo2() {
+        Thread t1 = new Thread(() -> {
+            for(int i = 0; i <= 100; i++){
+                System.out.println("----" + i);
+            }
+            System.out.println("t1 的中断标识位02 = "+ Thread.currentThread().isInterrupted());
+        }, "t1");
+        t1.start();
+        System.out.println("t1 的中断标识位为 "+ t1.isInterrupted());
+        //暂停毫秒
+        try {TimeUnit.MILLISECONDS.sleep(1);} catch (InterruptedException e) {e.printStackTrace();}
+        //被中断的线程，如果不监听中断状态并做出动作，interrupt 是不能中断线程的。
+        t1.interrupt();
+        System.out.println("t1 的中断标识位01 = "+ t1.isInterrupted());
+
+        //暂停毫秒
+        try {TimeUnit.MILLISECONDS.sleep(3);} catch (InterruptedException e) {e.printStackTrace();}
+        //线程已经打印完 100 ， 线程不处于活动状态，所以中断不会产生影响
+        System.out.println("t1 的中断标识位03 = "+ t1.isInterrupted());
+    }
+
+    private static void threadInterruptDemo() {
+        Thread t1 = new Thread(() -> {
+            while(true){
+                if(Thread.currentThread().isInterrupted()){
+                    System.out.println(Thread.currentThread().getName() + "\t isInterrupted() 被修改为 true ， 线程结束");
+                    break;
+                }
+                System.out.println("t1 ------ hello isInterrupted API");
+            }
+        }, "t1");
+        t1.start();
+
+        System.out.println("t1 的中断标识位为 "+ t1.isInterrupted());
+
+        //暂停毫秒
+        try {TimeUnit.MILLISECONDS.sleep(1);} catch (InterruptedException e) {e.printStackTrace();}
+        // 通过其他线程来中断，比如 t2 来中断 t1
+        //new Thread(() -> t1.interrupt(), "t2").start();
+        //t1 自己也可以中断 自己
+        t1.interrupt();
+
+        System.out.println("t1 的中断标识位为 "+ t1.isInterrupted());
     }
 
     private static void atomicBooleanDemo() {
@@ -22,7 +85,7 @@ public class InterruptDemo {
                     System.out.println(Thread.currentThread().getName() + "\t atomicBoolean 被修改为 true ， 线程结束");
                     break;
                 }
-                System.out.println("t1 ------ hello volatile");
+                System.out.println("t1 ------ hello atomicBoolean");
             }
         }, "t1").start();
 
